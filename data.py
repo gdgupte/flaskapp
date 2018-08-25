bd1caedd8b8d3dc4c227e57b7451b34849cbd04f
gdgupte <38730971+gdgupte@users.noreply.github.com>
2018-08-25 22:16:45 +0530

    Files added from local system

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One', 
+            'body': 'Ex exercitation id eiusmod dolor sint sunt magna aliqua ipsum ad ullamco consectetur reprehenderit anim. Incididunt commodo qui velit ut occaecat eiusmod occaecat irure. Ipsum labore enim et Lorem do et in Lorem adipisicing adipisicing anim. Laborum enim nulla sit ullamco dolore aliquip. Laboris labore ullamco consequat labore Lorem dolore ea dolore reprehenderit duis aute occaecat nisi elit. Nostrud duis in magna magna aute.',
+            'author': 'Gaurav Gupte',
+            'createdate': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two', 
+            'body': 'Ex exercitation id eiusmod dolor sint sunt magna aliqua ipsum ad ullamco consectetur reprehenderit anim. Incididunt commodo qui velit ut occaecat eiusmod occaecat irure. Ipsum labore enim et Lorem do et in Lorem adipisicing adipisicing anim. Laborum enim nulla sit ullamco dolore aliquip. Laboris labore ullamco consequat labore Lorem dolore ea dolore reprehenderit duis aute occaecat nisi elit. Nostrud duis in magna magna aute.',
+            'author': 'Gaurav Gupte',
+            'createdate': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three', 
+            'body': 'Ex exercitation id eiusmod dolor sint sunt magna aliqua ipsum ad ullamco consectetur reprehenderit anim. Incididunt commodo qui velit ut occaecat eiusmod occaecat irure. Ipsum labore enim et Lorem do et in Lorem adipisicing adipisicing anim. Laborum enim nulla sit ullamco dolore aliquip. Laboris labore ullamco consequat labore Lorem dolore ea dolore reprehenderit duis aute occaecat nisi elit. Nostrud duis in magna magna aute.',
+            'author': 'Gaurav Gupte',
+            'createdate': '04-25-2017'
+        },
+    ]
+    return articles
